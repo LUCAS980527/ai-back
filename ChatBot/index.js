92fd55e1a8887796e60f0d158f2f2b48891e9046
LUCAS980527 <amarbayn0527@gmail.com>
2025-12-23 21:48:16 +0800

    Update

--- ChatBot/index.js
+++ ChatBot/index.js
@@ -1,14 +1,18 @@
+const { InferenceClient } = require("@huggingface/inference");
+
 const ChatBot = async (req, res) => {
   const { message } = req.body;
-  console.log("message:", message);
+  //   console.log("message:", message);
 
   if (!message) {
     return res.status(400).json({ error: "Message is required!" });
   }
 
   try {
-    const result = await client.chatCompletion({
-      model: "mistralai/Mistral-7B-Instruct-v0.3",
+    const client = new InferenceClient(process.env.HF_TOKEN);
+
+    const chatCompletion = await client.chatCompletion({
+      model: "meta-llama/Llama-3.1-8B-Instruct:novita",
       messages: [
         {
           role: "user",
@@ -17,13 +21,12 @@ const ChatBot = async (req, res) => {
       ],
     });
 
-    if (!result?.choices?.length) {
-      throw new Error("No response from model");
-    }
+    const reply = chatCompletion.choices[0].message.content;
 
-    const reply = result.choices[0].message.content;
+    // console.log("Reply:", reply);
 
-    res.status(200).json({ reply });
+    // frontend руу буцаах
+    res.json({ reply });
   } catch (err) {
     console.error("HF ERROR:", err);
     res.status(500).json({
